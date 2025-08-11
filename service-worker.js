3a175a7cca64acaa3d1dfbf05b00d587afcd0307
PurushothMathav <purushoth.mathav@gmail.com>
2025-08-11 11:12:28 +0530

    streamify-1108251050

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "streamify-0608251150"; // ✅ Increment this on each update
+const CACHE_NAME = "streamify-1108251050"; // ✅ Increment this on each update
 const ASSETS_TO_CACHE = [
   "/streamify/",
   "/streamify/index.html",
