4bd110e55e0b6bc55abe8f54ec810ee7a25d94dc
PurushothMathav <purushoth.mathav@gmail.com>
2025-09-10 20:04:19 +0530

    1009250200

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "1009250100"; // ✅ Increment this on each update
+const CACHE_NAME = "1009250200"; // ✅ Increment this on each update
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
